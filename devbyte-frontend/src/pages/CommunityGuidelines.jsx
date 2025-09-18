8f023605175daab3d8fa3abea9a288e77b282ce1
Osita Chris Jerry <chrisjerry070@gmail.com>
2025-09-18 21:05:47 +0400

    Worked on footer component

---
+++ devbyte-frontend/src/pages/CommunityGuidelines.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer.jsx";
+
+const CommunityGuidelines = () => {
+  return (
+    <div className="min-h-screen">
+      <section className="py-20 w-full bg-gradient-to-r from-[#00AEEF] to-[#6A5DFF] ">
+        <h1>Community Guidelines</h1>
+      </section>
+      <Footer />
+    </div>
+  );
+};
+
+export default CommunityGuidelines;
--- devbyte-frontend/src/routes/AppRoutes.jsx
+++ devbyte-frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from '../pages/auth/Login.jsx'
 import Signup from '@/pages/auth/Signup.jsx'
+import CommunityGuidelines from '../pages/CommunityGuidelines.jsx';
 import App from '@/App.jsx'
 
 
@@ -11,6 +12,7 @@ export default function AppRoutes() {
       <Route path="/" element={<App/>} />
       <Route path="login" element= {<Login/>}/> 
       <Route path="signup" element= {<Signup/>}/> 
+      <Route path="communityGuidelines" element= {<CommunityGuidelines/>}/>
     </Routes>
   )
 }
