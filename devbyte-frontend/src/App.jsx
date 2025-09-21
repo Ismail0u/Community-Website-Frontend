75bf7d63b07c4421e6fd4b60096eb95409f72365
Darasimi <darasimi0603@gmail.com>
2025-09-21 17:36:10 +0100

    fix alignment issue on home page

--- devbyte-frontend/src/App.jsx
+++ devbyte-frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import AppRoutes from './routes/AppRoutes'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AppRoutes from "./routes/AppRoutes";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 export default function App() {
   return (
@@ -10,9 +10,8 @@ export default function App() {
       <h1>Welcome to DevByte Community</h1>
       <p>Select a page to continue.</p>
     </div>
-  )
+  );
 }
-
 
 // function App() {
 //   const [count, setCount] = useState(0)
