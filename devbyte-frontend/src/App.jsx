2bc1b6e575f7ba65cb7f721584e1369c844abccf
JENTAH REHEMA <jentahrehema7@gmail.com>
2025-09-22 22:37:32 +0300

    fix:fix app.jsx and about file

--- devbyte-frontend/src/App.jsx
+++ devbyte-frontend/src/App.jsx
@@ -1,13 +1,8 @@
 import AppRoutes from "./routes/AppRoutes";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 export default function App() {
   return (
     <div>
-      <h1>Welcome to DevByte Community</h1>
-      <p>Select a page to continue.</p>
       <AppRoutes />
     </div>
   );
